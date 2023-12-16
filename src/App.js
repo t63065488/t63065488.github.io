0137b055af78c08c2910c3bf6fcd7376bc8aba16
t63065488 <t63065488@gmail.com>
2023-12-16 13:22:41 +0000

    Initial TypeScript, navbar, tailwind css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Bar from './navbar/Bar.tsx'
 
 function App() {
   return (
     <div className="App">
+      <Bar/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
