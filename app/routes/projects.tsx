f076c892a259fb435b89d4f1f60034ca0b06a097
Thomas Millward <thomas.l.millward@gmail.com>
2025-12-28 23:23:28 +0000

    Add project route

---
+++ app/routes/projects.tsx
@@ -0,0 +1,11 @@
+import type { Route } from "./+types/home";
+
+export function meta(_: Route.MetaArgs) {
+  return [{ title: "Projects - Tom\'s Portfolio" }];
+}
+
+const Projects = (): React.ReactNode => {
+  return <></>;
+};
+
+export default Projects;
